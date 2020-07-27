38b08759c4aa942fd202de4c6bc63367c344e442
Scotzer <anais.sdc@gmail.com>
2020-07-27 14:52:35 +0200

    Moved todo code in Todo.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -71,46 +71,6 @@ class TodoList extends React.Component {
             return (
               <li>
               <Todo />
-                {this.state.editingTaskId !== task.id ? (
-                  <span>
-                    <span
-                      style={{
-                        textDecoration: task.active ? "none" : "line-through"
-                      }}
-                      onClick={() => this.toggleTask(task)}
-                    >
-                      {task.name}
-                    </span>
-                    <button onClick={() => this.deleteTodo(task.id)}>
-                      delete
-                    </button>
-                    <button onClick={() => this.editTask(task)}>edit</button>
-                  </span>
-                ) : (
-                  <span>
-                    <form
-                      onSubmit={e => {
-                        e.preventDefault();
-                        this.updateTask(task.id)
-                    }}
-                    >
-                      <input
-                        autoFocus
-                        value={this.state.editTaskName}
-                        onChange={e =>
-                          this.setState({ editTaskName: e.target.value })
-                        }
-                      />
-                      <button>Ok</button>
-                      <button
-                        type="button"
-                        onClick={() => this.setState({ editingTaskId: null })}
-                      >
-                        Cancel
-                      </button>
-                    </form>
-                  </span>
-                )}
               </li>
             );
           })}
