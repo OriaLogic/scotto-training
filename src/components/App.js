a3ec9d52bfc1827dd44cada354d47da66e2f9dc0
Scotzer <anais.sdc@gmail.com>
2020-07-28 19:02:23 +0200

    Added TodoList creation feature and disabled form buttons if empty input

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,27 @@ import TodoList from "./TodoList";
 // Small comment to open the PR
 class App extends React.Component {
   state = {
-    todoLists: [{id: 1, name: "List 1"}, {id: 2, name: "List 2"}]
+    todoLists: [],
+    newTodoListName: ""
+  }
+
+  createTodoList = () => {
+    if (this.state.todoLists.length === 0) {
+      this.setState({
+        todoLists: [{ id: 1, name: this.state.newTodoListName}],
+      });
+      return;
+    }
+
+      this.setState({
+        todoLists: [
+          ...this.state.todoLists,
+          {
+            id: this.state.todoLists[this.state.todoLists.length - 1].id + 1,
+            name: this.state.newTodoListName
+          }
+        ]
+    });
   }
 
   render() {
@@ -13,9 +33,28 @@ class App extends React.Component {
         <h1 className="main-title">Scotto TodoList App !</h1>
         <div className="app-container">
           <h2 style={{ marginBottom: 30 }}>Number of lists: {this.state.todoLists.length}</h2>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              if (this.state.newTodoListName === "") return;
+              this.createTodoList();
+              this.setState({newTodoListName: ""})
+            }}
+          >
+            <input
+              autoFocus
+              placeholder="Enter a TodoList name"
+              value={this.state.newTodoListName}
+              onChange={e => this.setState({newTodoListName: e.target.value})}
+            />
+            <button type="submit" disabled={this.state.newTodoListName === ""}>Submit</button>
+          </form>
           <div className="all-todoLists-container">
-            <TodoList name={this.state.todoLists[0].name} />
-            <TodoList name={this.state.todoLists[1].name} />
+            {this.state.todoLists.map((todoList) => {
+              return (
+                <TodoList name={todoList.name}/>
+              )
+            })}
           </div>
         </div>
       </div>
--- src/components/TodoCreationForm.js
+++ src/components/TodoCreationForm.js
@@ -10,16 +10,18 @@ export default class TodoCreationForm extends React.Component {
       <form
         onSubmit={e => {
           e.preventDefault();
+          if (this.state.taskName === "") return;
           this.props.onCreate(this.state.taskName)
           this.setState({taskName: ""})
         }}
       >
         <input
           autoFocus
+          placeholder="Enter a task name"
           value={this.state.taskName}
           onChange={e => this.setState({taskName: e.target.value})}
         />
-        <button type="submit">Submit</button>
+        <button disabled={this.state.taskName === ""} type="submit">Submit</button>
       </form>
     )
   }
