92e4095869b24cdb0220f97def50f9dc901d1de0
Scotzer <anais.sdc@gmail.com>
2020-08-12 15:17:15 +0200

    Step 4

---
+++ src/redux/reducers/todoLists.js
@@ -0,0 +1,17 @@
+const initialState = {
+  1: {
+     id: 1,
+     name: "General todolist",
+     todos: {
+       1: {
+         id: 1,
+         name: "First todo",
+         active: true
+       }
+     }
+  }
+}
+
+const todoLists = () => {
+  
+}
