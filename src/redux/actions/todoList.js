504e92a86fa3e58252fafc4f623877500ae5e39f
Scotzer <anais.sdc@gmail.com>
2020-08-16 14:16:05 +0200

    Added DELETE_TODOLIST in actions todoList

--- src/redux/actions/todoList.js
+++ src/redux/actions/todoList.js
@@ -1,10 +1,20 @@
 import { ADD_TODOLIST } from "../ActionTypes"
+import { DELETE_TODOLIST } from "../ActionTypes"
 
 export const addTodoList = (name) => {
   return {
     type: ADD_TODOLIST,
     payload: {
       name
+    }
+  }
+}
+
+export const deleteTodoList = (todoListId) => {
+  return {
+    type: DELETE_TODOLIST,
+    payload: {
+      todoListId
     }
   }
 }
