6787f03c1f5b471e406f21aabdf25a539c225593
Bonnet <lucas.lbonnet@gmail.com>
2020-09-02 18:22:24 +0200

    Add redux devtools extension (#10)
    Co-authored-by: Lucas Bonnet <lucas@elevo.io>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
-import { createStore } from "redux"
-import todoLists from "./reducers/todoLists"
+import { createStore } from "redux";
+import todoLists from "./reducers/todoLists";
 
-const initialState = JSON.parse(localStorage.getItem("dataState"))
+const initialState = JSON.parse(localStorage.getItem("dataState"));
 console.log(initialState);
 
-const store = createStore(todoLists, initialState)
+const store = createStore(
+  todoLists,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-export default store
+export default store;
