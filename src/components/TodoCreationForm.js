d78d4af5a8235594c419907380951ac11edca311
Scotzer <anais.sdc@gmail.com>
2020-09-12 17:06:40 +0200

    Imported datepicker in TodoCreationForm.js

--- src/components/TodoCreationForm.js
+++ src/components/TodoCreationForm.js
@@ -1,4 +1,7 @@
 import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 export default class TodoCreationForm extends React.Component {
   state = {
