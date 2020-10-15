5fa5c84e47ad8d98f30d478fc8d428b2c2a4742d
Scotzer <anais.sdc@gmail.com>
2020-10-15 10:10:55 -1000

    Added initialState to notifications reducer

--- src/redux/reducers/notifications.js
+++ src/redux/reducers/notifications.js
@@ -1,5 +1,33 @@
 
 const initialState = {
+  
+  1: {
+    id: 1,
+    active: true,
+    todo: {
+      id: 1,
+      name: "code all day long",
+      todoListId: 1
+    }
+  },
+  2:  {
+    id: 2,
+    active: false,
+    todo: {
+      id: 2,
+      name: "pakaLolo all day long",
+      todoListId: 2
+    }
+  },
+  3:  {
+    id: 3,
+    active: true,
+    todo: {
+      id: 3,
+      name: "plongée all day long",
+      todoListId: 3
+    }
+  }
 
 }
 
