3346f2fcadaeffed08f15e1bf715bbffe29e12df
Scotzer <anais.sdc@gmail.com>
2020-10-25 16:45:57 -1000

    Improved notification component

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Notification() {
     return (
-      <div className="notification">WESH LA FAMILLE
+      <div className="notification is-warning">
+      <button className="delete"></button>
+      Todo is overdue!
+      <button className="button is-white is-small">Deactivate</button>
       </div>
     )
 }
