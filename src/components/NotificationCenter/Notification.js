c67feba2585ca55ba0a43fd3e0ad6dea5d056285
Scotzer <anais.sdc@gmail.com>
2020-10-29 11:29:47 -1000

    Added css to style notifications

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function Notification({onDismiss, onDeactivate, notification}) {
     return (
       <div className="notification is-warning">
-      <button className="delete" onClick={onDismiss} ></button>
-      {notification.todo.name} is overdue!
-      <button className="button is-white is-small" onClick={onDeactivate}>Deactivate</button>
+        <button className="delete" onClick={onDismiss} ></button>
+        {notification.todo.name} is overdue!
+        <span className="notification-buttons">
+          <button className="button is-white is-small" onClick={onDeactivate}>Deactivate</button>
+        </span>
       </div>
     )
 }
