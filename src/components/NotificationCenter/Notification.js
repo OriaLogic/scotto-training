cff547758be8dd7052c98c23d8a3163cddae1f37
Scotzer <anais.sdc@gmail.com>
2020-10-28 17:57:03 -1000

    Connected deactivate button to action in Notification component

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Notification({onDismiss}) {
+export default function Notification({onDismiss, onDeactivate}) {
     return (
       <div className="notification is-warning">
       <button className="delete" onClick={onDismiss} ></button>
       Todo is overdue!
-      <button className="button is-white is-small">Deactivate</button>
+      <button className="button is-white is-small" onClick={onDeactivate}>Deactivate</button>
       </div>
     )
 }
