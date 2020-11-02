3d069782a41bcff14b15fefb665165a955715a72
Scotzer <anais.sdc@gmail.com>
2020-11-01 20:17:17 -1000

    Changed deactivate button text to 'mark as done'

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -6,7 +6,7 @@ export default function Notification({onDismiss, onDeactivate, notification}) {
         <button className="delete" onClick={onDismiss} ></button>
         {notification.todo.name} is overdue!
         <div className="notification-buttons">
-          <button className="button is-white is-small" onClick={onDeactivate}>Deactivate</button>
+          <button className="button is-white is-small" onClick={onDeactivate}>Mark as done</button>
         </div>
       </div>
     )
