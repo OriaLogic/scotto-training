50c855e71dafedf176a633540667d8cc96206805
Scotzer <anais.sdc@gmail.com>
2020-11-04 16:10:07 -1000

    Added snooze dropdown in notification component

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Dropdown, { DropdownItem } from '../library/Dropdown';
 
 export default function Notification({onDismiss, onDeactivate, notification}) {
     return (
@@ -7,6 +8,14 @@ export default function Notification({onDismiss, onDeactivate, notification}) {
         {notification.todo.name} is overdue!
         <div className="notification-buttons">
           <button className="button is-white is-small" onClick={onDeactivate}>Mark as done</button>
+
+          <Dropdown>
+            <DropdownItem onClick={() => console.log('snooze 1 day')}>Snooze 1 day</DropdownItem>
+            <DropdownItem onClick={() => console.log('snooze 3 days')}>Snooze 3 days</DropdownItem>
+            <DropdownItem onClick={() => console.log('snooze 1 week')}>Snooze 1 week</DropdownItem>
+            <DropdownItem onClick={() => console.log('snooze 3 weeks')}>Snooze 3 weeks</DropdownItem>
+          </Dropdown>
+
         </div>
       </div>
     )
