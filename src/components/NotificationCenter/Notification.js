4486b04546ce4d54718986e43b58e610a34a1ac4
Scotzer <anais.sdc@gmail.com>
2020-11-04 17:40:53 -1000

    Plugged snoozeTodo function to Notification snooze actions

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Dropdown, { DropdownItem } from '../library/Dropdown';
 
-export default function Notification({onDismiss, onDeactivate, notification}) {
+export default function Notification({onDismiss, onDeactivate, notification, onSnooze}) {
     return (
       <div className="notification is-warning">
         <button className="delete" onClick={onDismiss} ></button>
@@ -10,10 +10,10 @@ export default function Notification({onDismiss, onDeactivate, notification}) {
           <button className="button is-white is-small" onClick={onDeactivate}>Mark as done</button>
 
           <Dropdown>
-            <DropdownItem onClick={() => console.log('snooze 1 day')}>Snooze 1 day</DropdownItem>
-            <DropdownItem onClick={() => console.log('snooze 3 days')}>Snooze 3 days</DropdownItem>
-            <DropdownItem onClick={() => console.log('snooze 1 week')}>Snooze 1 week</DropdownItem>
-            <DropdownItem onClick={() => console.log('snooze 3 weeks')}>Snooze 3 weeks</DropdownItem>
+            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 1)}>Snooze 1 day</DropdownItem>
+            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 3)}>Snooze 3 days</DropdownItem>
+            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 7)}>Snooze 1 week</DropdownItem>
+            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 21)}>Snooze 3 weeks</DropdownItem>
           </Dropdown>
 
         </div>
