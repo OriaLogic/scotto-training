2ccf2b189230a9d1db1837035f9e9a4ac66f326f
Scotzer <anais.sdc@gmail.com>
2020-11-04 18:48:40 -1000

    Added a button for snooze todo in notifications

--- src/components/NotificationCenter/Notification.js
+++ src/components/NotificationCenter/Notification.js
@@ -9,13 +9,17 @@ export default function Notification({onDismiss, onDeactivate, notification, onS
         <div className="notification-buttons">
           <button className="button is-white is-small" onClick={onDeactivate}>Mark as done</button>
 
-          <Dropdown>
-            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 1)}>Snooze 1 day</DropdownItem>
-            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 3)}>Snooze 3 days</DropdownItem>
-            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 7)}>Snooze 1 week</DropdownItem>
-            <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 21)}>Snooze 3 weeks</DropdownItem>
-          </Dropdown>
-
+          <button className="button snooze is-white is-small">
+          <span className="icon is-small">
+            <i class="fas fa-clock"></i>
+          </span>
+            <Dropdown>
+              <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 1)}>Snooze 1 day</DropdownItem>
+              <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 3)}>Snooze 3 days</DropdownItem>
+              <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 7)}>Snooze 1 week</DropdownItem>
+              <DropdownItem onClick={() => onSnooze(notification.id, notification.todoListId, notification.todo, 21)}>Snooze 3 weeks</DropdownItem>
+            </Dropdown>
+          </button>
         </div>
       </div>
     )
