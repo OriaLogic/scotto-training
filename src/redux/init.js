5dccd359dcd8e35438e0918c954f787f0a5d4112
Scotzer <anais.sdc@gmail.com>
2020-11-25 18:32:37 -1000

    Moved initialState and timeout notifications to init.js

--- src/redux/init.js
+++ src/redux/init.js
@@ -1,11 +1,48 @@
+const storedState = JSON.parse(localStorage.getItem("dataState"));
+const todoListsState = storedState.todoLists;
+
 export const getInitialState = () => {
-  //return initialState as before
+  Object.keys(todoListsState).forEach(todoListId => {
+    const todoList = todoListsState[todoListId];
+    const todos = todoList.todos;
+
+    Object.keys(todos).forEach(todoId => {
+      const todo = todos[todoId];
+      todo.dueDate = new Date(todo.dueDate);
+    })
+  })
+
+  const initialState = {
+    todoLists: todoListsState,
+    userPreferences: storedState.userPreferences,
+    notifications: { }
+  }
+
+  return initialState
 };
 
-export const afterStoreInitialization = () => {
+export const afterStoreInitialization = store => {
   setTimeout(() => triggerNotifications(store), 5000);
 };
 
 const triggerNotifications = store => {
-  //code that dispatch ADD_NOTIFICATION actions
+  setTimeout(() => {
+    Object.keys(todoListsState).forEach(todoListId => {
+      const todoList = todoListsState[todoListId];
+      const todos = todoList.todos;
+
+      Object.keys(todos).forEach(todoId => {
+        const todo = todos[todoId];
+        if (todo.active && todo.dueDate < new Date()) {
+          store.dispatch({
+            type: 'ADD_NOTIFICATION',
+            payload: {
+              todo,
+              todoListId
+            }
+          })
+        }
+      })
+    })
+  }, 5000)
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,24 +2,7 @@ import { createStore } from "redux";
 import rootReducer from './reducers/root';
 import { getInitialState, afterStoreInitialization } from "./init";
 
-const storedState = JSON.parse(localStorage.getItem("dataState"));
-const todoListsState = storedState.todoLists;
-
-Object.keys(todoListsState).forEach(todoListId => {
-  const todoList = todoListsState[todoListId];
-  const todos = todoList.todos;
-
-  Object.keys(todos).forEach(todoId => {
-    const todo = todos[todoId];
-    todo.dueDate = new Date(todo.dueDate);
-  })
-})
-
-const initialState = {
-  todoLists: todoListsState,
-  userPreferences: storedState.userPreferences,
-  notifications: { }
-}
+const initialState = getInitialState();
 
 const store = createStore(
   rootReducer,
@@ -27,25 +10,6 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-setTimeout(() => {
-  Object.keys(todoListsState).forEach(todoListId => {
-    const todoList = todoListsState[todoListId];
-    const todos = todoList.todos;
-
-    Object.keys(todos).forEach(todoId => {
-      const todo = todos[todoId];
-      if (todo.active && todo.dueDate < new Date()) {
-        store.dispatch({
-          type: 'ADD_NOTIFICATION',
-          payload: {
-            todo,
-            todoListId
-          }
-        })
-      }
-    })
-  })
-}, 5000)
-
+afterStoreInitialization(store);
 
 export default store;
