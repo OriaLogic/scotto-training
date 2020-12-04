d477669ae2f75b3b81232705fdd9052f05388eec
Scotzer <anais.sdc@gmail.com>
2020-12-03 15:14:23 -1000

    Deleted "more" menu and "sign up" on the navbar and

--- src/components/AppLayout/Header.js
+++ src/components/AppLayout/Header.js
@@ -27,35 +27,11 @@ export default class Header extends React.Component {
               Documentation
             </a>
 
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">
-                More
-              </a>
-
-              <div className="navbar-dropdown">
-                <a className="navbar-item">
-                  About
-                </a>
-                <a className="navbar-item">
-                  Jobs
-                </a>
-                <a className="navbar-item">
-                  Contact
-                </a>
-                <hr className="navbar-divider"/>
-                <a className="navbar-item">
-                  Report an issue
-                </a>
-              </div>
-            </div>
           </div>
 
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
                 <a className="button is-light">
                   Log in
                 </a>
