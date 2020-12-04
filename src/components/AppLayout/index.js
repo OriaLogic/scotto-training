9fb6c9685f6a2cd669545adfe1a153e5477b5790
Scotzer <anais.sdc@gmail.com>
2020-12-03 14:53:39 -1000

    Imported navbar code in Header component

--- src/components/AppLayout/index.js
+++ src/components/AppLayout/index.js
@@ -3,16 +3,20 @@ import Header from "./Header";
 import NotificationCenter from "../NotificationCenter";
 
 export default class AppLayout extends React.Component {
+
   render() {
-    <div className="App">
 
-      <Header />
-      <NotificationCenter />
+    return (
+      <div className="App">
 
-      <div className="app-container">
-        {this.props.children}
+        <Header />
+        <NotificationCenter />
+
+        <div className="app-container">
+          {this.props.children}
+        </div>
+
       </div>
-
-    </div>
+    )
   }
 }
