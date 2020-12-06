e9af3e18fd0f9f9d27ef82a8de4793871b751cb4
Scotzer <anais.sdc@gmail.com>
2020-12-05 18:22:41 -1000

    Splitted screen in Note.js and added menu

--- src/components/pages/Notes/GroupNav.js
+++ src/components/pages/Notes/GroupNav.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-export default function GroupNav({ props }) {
+export default function GroupNav({ children }) {
   return (
-    <div className="group-nav">GroupNav</div>
+    <div className="group-nav">
+      <div className="columns">
+        <div className="column is-2">
+          <aside class="menu">
+            <ul class="menu-list">
+              <li><a>Notes</a></li>
+              <li><a>Personal notes</a></li>
+            </ul>
+          </aside>
+        </div>
+        <div className="column is-10">
+          {children}
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/pages/Notes/index.js
+++ src/components/pages/Notes/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import GroupNav from "./GroupNav";
 
 export default function Notes({ props }) {
   return (
-    <div className="notes-container">Hi notes!</div>
+    <div className="notes-container">
+      <GroupNav />
+    </div>
   )
 }
