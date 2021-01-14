0ae7fefccc99b408467c1aa0031ae35db1e5342b
Scotzer <anais.sdc@gmail.com>
2021-01-14 12:54:20 -1000

    Separated the content of Note component in 2 columns

--- src/components/pages/Notes/NoteNav.js
+++ src/components/pages/Notes/NoteNav.js
@@ -1,1 +1,15 @@
 import React, { useState } from 'react';
+
+export default function Note( {children} ) {
+  return(
+    <div className="note-nav">
+      <div className="columns">
+        <div className="column is-3">
+        </div>
+        <div className="column is-9">
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
