6632c28bf4feb79e8e5d609100fd3df7fb22c7dd
Scotzer <anais.sdc@gmail.com>
2021-02-10 16:23:59 -1000

    Created Note component and displayed notes and noteList from idex.js

--- src/components/pages/Notes/NoteNav.js
+++ src/components/pages/Notes/NoteNav.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-export default function Note( {children} ) {
+export default function NoteNav({ children }) {
   return(
     <div className="note-nav">
       <div className="columns">
         <div className="column is-3">
+          Note list
         </div>
         <div className="column is-9">
           {children}
--- src/components/pages/Notes/index.js
+++ src/components/pages/Notes/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+
 import NoteGroupNav from "./NoteGroupNav";
+import NoteNav from "./NoteNav";
+import Note from "./Note";
 
 export default function Notes({ props }) {
   return (
     <div className="notes-container">
       <NoteGroupNav>
-        “The notes of the group”
+        <Route
+          path="/notes/:groupId"
+          render={({ match }) => {
+            return (
+              <NoteNav
+                match={match}
+                groupId={match.params.groupId}
+              >
+                <Route
+                  path='/notes/:groupId/:noteId'
+                  component={Note}
+                />
+              </NoteNav>
+            );
+          }}
+        />
       </NoteGroupNav>
     </div>
   )
