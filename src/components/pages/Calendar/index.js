6d2135bcd10a0c0d658cf6b9236d7f60be32c433
Scotzer <anais.sdc@gmail.com>
2021-05-07 18:43:06 -1000

    Created days columns

--- src/components/pages/Calendar/index.js
+++ src/components/pages/Calendar/index.js
@@ -4,7 +4,13 @@ export default function Calendar () {
   return(
     <div className="main-container">
       <div className="calendar-container">
-        Hi!
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+        <div>Sunday</div>
       </div>
     </div>
   )
