9abdd96abba92da13a3f15da46f2b9a773bc0fb7
Scotzer <anais.sdc@gmail.com>
2021-05-16 18:34:02 -1000

    Fixed calendar css

--- src/components/pages/Calendar/index.js
+++ src/components/pages/Calendar/index.js
@@ -4,25 +4,63 @@ import Topbar from "./Topbar";
 
 export default function Calendar () {
   return(
-    <div className="calendar-container">
+    <div className="calendar">
       <Topbar />
-      <div className="days">
-        <div>Monday</div>
-        <div>Tuesday</div>
-        <div>Wednesday</div>
-        <div>Thursday</div>
-        <div>Friday</div>
-        <div>Saturday</div>
-        <div>Sunday</div>
-      </div>
-      <div className="cells">
-        <div>aa</div>
-        <div>bb</div>
-        <div>cc</div>
-        <div>dd</div>
-        <div>ee</div>
-        <div>ff</div>
-        <div>gg</div>
+      <div className="view month-days">
+        <div className="weekdays">
+          <div className="day">Monday</div>
+          <div className="day">Tuesday</div>
+          <div className="day">Wednesday</div>
+          <div className="day">Thursday</div>
+          <div className="day">Friday</div>
+          <div className="day">Saturday</div>
+          <div className="day">Sunday</div>
+        </div>
+        <div className="week">
+          <div className="day">aa</div>
+          <div className="day">bb</div>
+          <div className="day">cc</div>
+          <div className="day">dd</div>
+          <div className="day">ee</div>
+          <div className="day">ff</div>
+          <div className="day">gg</div>
+        </div>
+        <div className="week">
+          <div className="day">aa</div>
+          <div className="day">bb</div>
+          <div className="day">cc</div>
+          <div className="day">dd</div>
+          <div className="day">ee</div>
+          <div className="day">ff</div>
+          <div className="day">gg</div>
+        </div>
+        <div className="week">
+          <div className="day">aa</div>
+          <div className="day">bb</div>
+          <div className="day">cc</div>
+          <div className="day">dd</div>
+          <div className="day">ee</div>
+          <div className="day">ff</div>
+          <div className="day">gg</div>
+        </div>
+        <div className="week">
+          <div className="day">aa</div>
+          <div className="day">bb</div>
+          <div className="day">cc</div>
+          <div className="day">dd</div>
+          <div className="day">ee</div>
+          <div className="day">ff</div>
+          <div className="day">gg</div>
+        </div>
+        <div className="week">
+          <div className="day">aa</div>
+          <div className="day">bb</div>
+          <div className="day">cc</div>
+          <div className="day">dd</div>
+          <div className="day">ee</div>
+          <div className="day">ff</div>
+          <div className="day">gg</div>
+        </div>
       </div>
     </div>
   )
