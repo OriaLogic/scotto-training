a78e421083d0f6fba5bbfefaef4c8e0271b44bfd
Scotzer <anais.sdc@gmail.com>
2021-06-03 17:03:25 -1000

    Imported moment.js and displayed current month in header

--- src/components/pages/Calendar/Topbar.js
+++ src/components/pages/Calendar/Topbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 export default function Topbar ({ }) {
+  const month = moment().format('MMMM');
+
   return (
     <div className="header">
-      MAY
+      {month}
     </div>
   )
 }
